8cfd36b2ef8643743c00ad81aec46a299fd2a38b
kabynks <kabynks@gmail.com>
2024-03-14 11:26:59 -0700

    change db

--- src/settings/production.py
+++ src/settings/production.py
@@ -3,11 +3,7 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config("DB_NAME"),
-        "USER": config("DB_USER"),
-        "PASSWORD": config("DB_PASSWORD"),
-        "HOST": config("DB_HOST"),
-        'PORT': 5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'production.sqlite3',
     }
 }
